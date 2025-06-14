9db9f8f10c0e47ef44ef26daa7e8b58077d5b2fd
rlatkdgus2627 <rlatkdgus2627@gmail.com>
2025-06-14 15:57:50 +0900

    fix : LoLA import 추가

--- modules/lora.py
+++ modules/lora.py
@@ -1,3 +1,7 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
 class LoRALinear(nn.Module):
     def __init__(self, in_features, out_features, r=4, alpha=1.0, bias=True):
         super().__init__()
